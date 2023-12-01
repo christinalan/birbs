b2936fc552a569b89bd2766ed3390fe135162f1c
christina lan <christina.lan13@gmail.com>
2023-12-01 11:36:28 -0500

    added canada options

--- src/components/regionSelection.tsx
+++ src/components/regionSelection.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, FC } from 'react';
-import regionData from '../data/USregions.json';
+import { useState, useEffect, ChangeEvent, FC } from 'react';
+import USregionData from '../data/USregions.json';
+import CAregionData from '../data/CAregions.json'
 
 interface RegionSelectionProps {
     onSelect: (selectedRegion: Region | null) => void;
@@ -10,22 +11,52 @@ interface Region {
     name: string;
 }
 
-const RegionSelection: FC<RegionSelectionProps> = ({onSelect}) => {
-    const regions: Region[] = regionData.data;
-    const regionName = regions.map((region: Region) => <option key={region.abbreviation}>{region.name}</option>)
 
+const RegionSelection: FC<RegionSelectionProps> = ({onSelect}) => {
+    const [country, setCountry] = useState("");
+    const [regions, setRegions] = useState<Region[]>([]);
+    const [regionNames, setRegionNames] = useState<HTMLOptionElement[]>();
+    //setting US regions
+    const usRegions: Region[] = USregionData.data;
+    const caRegions: Region[] = CAregionData.data;
+    
+    const handleCountryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        event.preventDefault();
+        const selectedCountry = event.target.value;
+        setCountry(selectedCountry);
+    }
+    
+    useEffect(() => {
+        if (country === "US") {
+            setRegions(usRegions)
+            
+        } else if (country === "CA") {
+            setRegions(caRegions);
+        }
+    }, [country])
+    
+    
+
+    
     const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
         event.preventDefault();
         const selectedOption = event.target.value;
-        const selectedRegion = regions.find((region: Region) => region.name === selectedOption)
+        const selectedRegion = usRegions.find((region: Region) => region.name === selectedOption)
         onSelect(selectedRegion || null)
     }
 
     return (
         <div className="region-dropdown">
+            <select name="countries" value={country} onChange={handleCountryChange}>
+                <option value="none">Please select a country</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+            </select>
                 <select name="regions" onChange={handleSelectChange}>
                     <option value="none">Please select a region</option>
-                    {regionName}
+                    {regions.map((region: Region) => (
+                        <option key={region.abbreviation} value={region.name}>{region.name}</option>
+                    ))}
                 </select>
         </div>
     )
